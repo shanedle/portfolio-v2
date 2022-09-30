cc3d844f290242bd9d13579a961275cc1914ea63
Shane <31248360+shanedle@users.noreply.github.com>
2022-09-30 12:45:16 +0200

    :speech_balloon: footer text update

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -3,11 +3,7 @@ export default function Footer() {
     <footer className="bg-zinc-900 inset-x-0 bottom-0 min-w-full">
       <div className="md:max-w-4xl xl:max-w-6xl mx-auto text-rose-200 flex justify-center items-center py-4">
         <div>
-          &copy; {new Date().getFullYear()}, Made with{" "}
-          <span role="img" aria-label="heart">
-            ❤️
-          </span>{" "}
-          by Shane Le
+          &copy; {new Date().getFullYear()}, Designed and hand-coded by Shane Le
         </div>
       </div>
     </footer>
